b5f8d7eabedd28af67f0063f5be511c2f8131d1e
Moritz Sudhof <moritz.sudhof@gmail.com>
2014-02-27 15:56:21 -0800

    alternate home page: call-to-action

--- routes/user.js
+++ routes/user.js
@@ -55,6 +55,26 @@ exports.create_new_profile = function(req, res) {
     });
 }
 
+/* POST - Handles posting of user data */
+exports.handle_create_profile = function(req, res) {
+    var user = req.body.user;
+    user.id = parseInt(user.id);
+    user_data.update_user(user);
+    // Add a status message about what happened
+    var status_messages = [{"text": "New profile created.", "class": "success-message", "glyphicon": "glyphicon-ok"}];
+    req.session.status_messages = status_messages;
+
+    // this is a real user now
+    req.session.curr_user_id = user.id;
+    req.session.username = user.first_name;
+    // let the homepage know this user is new
+    req.session.new_user = true;
+
+    // redirect to home page
+    res.redirect("/");
+    return; 
+};
+
 
 /* POST - Handles posting of user data */
 exports.handle_update_profile = function(req, res) {
